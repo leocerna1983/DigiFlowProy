b20c68d8f52713ac28200a96ac110ee5f55fdc3e
LeoCerna <leonidas.cerna@gmail.com>
2018-04-08 16:52:54 -0400

    Cambio en dll para procesamiento de txt en proyecto windows mobile 5
    Cambio de dll para procesamiento de txt en proyecto windows mobilw 5

---
+++ PruebaWindowsMobile/PruebaWindowsMobile/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+using TraductorTxt;
+
+namespace SmartDeviceProject1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Traductor trad = new Traductor(@"TraductorXMLite.sqlite3", 1);
+            if (trad.VerificarConexion())
+            {
+                CampoCalculado CampCalculado = new CampoCalculado();                
+                string ruta = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase) + "\\" + "1.txt";
+                FileStream stream = File.OpenRead(ruta);
+                byte[] fileBytes = new byte[stream.Length];
+                stream.Read(fileBytes, 0, fileBytes.Length);
+                stream.Close();
+                trad.Procesar(fileBytes);
+                Console.WriteLine("Conexion valida.");
+            }
+            else
+            {
+                Console.WriteLine("Conexion invalida.");
+            }
+            Console.ReadLine();          
+        }
+
+        public static void CopyStream(Stream input, Stream output)
+        {
+            byte[] buffer = new byte[32 * 1024]; // Or whatever size you want
+            int read;
+            while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                output.Write(buffer, 0, read);
+            }
+        }
+    }
+}
---
+++ PruebaWindowsMobile/TraductorTxt/CuerpoC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TraductorTxt
+{
+    public class CuerpoC
+    {
+        public int NroLinDR { get; set; }
+        public string TpoMov { get; set; }
+        public decimal ValorDR { get; set; }
+
+    }
+}
